5f36a327ae450d281b48b87d56b595e09d528687
Collin Sage <Collin Sage@141.114.165.143>
2013-02-27 18:44:42 -0500

    Trying to get the stupid box to point at the mouse.

---
+++ FuzzyVampires/src/client/Player.java
@@ -0,0 +1,179 @@
+package client;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
+import org.newdawn.slick.geom.Vector2f;
+
+public class Player {
+
+	private float		xspeed	= .25F, yspeed = .25F;
+	private double		theta;
+	private boolean		left, right, up, down;
+
+	private Vector2f	playerdirection;
+	private Shape		newhitbox;
+	private Shape		hitbox;
+
+	public Player()
+	{
+		this.hitbox = new Rectangle(50, 50, 25, 25);
+
+		this.setX(Game.WIDTH / 2 - hitbox.getWidth() / 2);
+		this.setY(Game.HEIGHT / 2 - hitbox.getHeight() / 2);
+
+		System.out.println(this.getX() + " " + this.getY());
+		playerdirection = new Vector2f(0, 1);
+	}
+
+	public void render(Graphics g)
+	{
+		g.setColor(Color.white);
+
+		// g.draw(hitbox);
+		g.draw(newhitbox);
+		g.drawLine(hitbox.getCenterX(), hitbox.getCenterY(), Mouse.getX(),
+				Game.HEIGHT - Mouse.getY());
+
+		g.drawLine(hitbox.getCenterX() + dx, hitbox.getCenterY(),
+				hitbox.getCenterX() + dx, hitbox.getCenterY() + dy);
+		g.drawLine(hitbox.getCenterX(), hitbox.getCenterY(),
+				hitbox.getCenterX() + dx, hitbox.getCenterY());
+		
+		g.drawString("" + dy, hitbox.getCenterX() + dx, hitbox.getCenterY() + dy / 2);
+		g.drawString("" + dx, hitbox.getCenterX() + dx / 2, hitbox.getCenterY());
+
+		g.fillOval(Game.WIDTH / 2 - 2, Game.HEIGHT / 2 - 2, 4, 4);
+		g.drawString(
+				"Player coords: (" + hitbox.getCenterX() + ", "
+						+ hitbox.getCenterY() + ")", 25, 50);
+		g.drawString("Mouse coords: " + Mouse.getX() + ", "
+				+ (Game.HEIGHT - Mouse.getY()), 25, 35);
+		g.drawString("Player direction: " + playerdirection.x + " "
+				+ playerdirection.y, 25, 65);
+		g.drawString("Theta: " + theta + " " + theta * 180 / Math.PI, 25, 80);
+		g.drawString("Components: " + dy + "y " + dx + "x", 25, 95);
+	}
+
+	public void update(Input input, int delta)
+	{
+		playerdirection = calculateDirectionVector(input);
+
+		Vector3f playerdirectionas3f = new Vector3f(playerdirection.x,
+				playerdirection.y, 0);
+		Vector3f horizon = new Vector3f(1, 0, 0);
+
+		theta = convertTheta(playerdirectionas3f, horizon);
+
+		newhitbox = hitbox.transform(Transform.createRotateTransform(
+				(float) theta, hitbox.getCenterX(), hitbox.getCenterY()));
+		setDirections();
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_RETURN))
+		{
+			this.setX(Game.WIDTH / 2 - hitbox.getWidth() / 2);
+			this.setY(Game.HEIGHT / 2 - hitbox.getHeight() / 2);
+		}
+
+		float dx = 0, dy = 0;
+
+		if (left)
+		{
+			dx += -xspeed;
+		}
+		if (right)
+		{
+			dx += xspeed;
+		}
+		if (up)
+		{
+			dy += -yspeed;
+		}
+		if (down)
+		{
+			dy += yspeed;
+		}
+		move(dx * delta, dy * delta);
+	}
+
+	public void move(float dx, float dy)
+	{
+		this.incrementX(dx);
+		this.incrementY(dy);
+	}
+
+	private void setDirections()
+	{
+
+		left = Keyboard.isKeyDown(Keyboard.KEY_LEFT);
+		right = Keyboard.isKeyDown(Keyboard.KEY_RIGHT);
+		up = Keyboard.isKeyDown(Keyboard.KEY_UP);
+		down = Keyboard.isKeyDown(Keyboard.KEY_DOWN);
+	}
+
+	float	dx;
+	float	dy;
+
+	private Vector2f calculateDirectionVector(Input input)
+	{
+		float mousex = input.getMouseX();
+		float mousey = Game.HEIGHT - input.getMouseY();
+
+		dx = mousex - (this.getX() + hitbox.getWidth() / 2);
+		dy = -1 * (mousey - (this.getY() + hitbox.getHeight() / 2));
+
+		return new Vector2f(dx, dy).normalise();
+	}
+
+	private double convertTheta(Vector3f player, Vector3f horizon)
+	{
+		double angle = Vector3f.angle(player, horizon);
+
+		if(angle < 0)
+		{
+			angle = -angle;
+		}
+		return angle;
+	}
+
+	public void onKey(int keycode)
+	{
+
+	}
+
+	public void incrementX(float dx)
+	{
+		this.hitbox.setX(hitbox.getX() + dx);
+	}
+
+	public void setX(float x)
+	{
+		this.hitbox.setX(x);
+	}
+
+	public float getX()
+	{
+		return hitbox.getX();
+	}
+
+	public void incrementY(float dy)
+	{
+		this.hitbox.setY(hitbox.getY() + dy);
+	}
+
+	public void setY(float y)
+	{
+		this.hitbox.setY(y);
+	}
+
+	public float getY()
+	{
+		return hitbox.getY();
+	}
+}
--- FuzzyVampires/src/client/StateInGame.java
+++ FuzzyVampires/src/client/StateInGame.java
@@ -1,48 +1,46 @@
 package client;
 
-	import org.newdawn.slick.GameContainer;
-	import org.newdawn.slick.Graphics;
-	import org.newdawn.slick.SlickException;
-	import org.newdawn.slick.state.BasicGameState;
-	import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
-	public class StateInGame extends BasicGameState {
+public class StateInGame extends BasicGameState {
 
-		private int stateID;
-		
-		public StateInGame(int stateID)
-		{
-			this.stateID = stateID;
-		}
-		
-		@Override
-		public void init(GameContainer gc, StateBasedGame sbg)
-				throws SlickException
-		{
-			// TODO Auto-generated method stub
-			
-		}
+	private int		stateID;
+	private Player	player;
 
-		@Override
-		public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
-				throws SlickException
-		{
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void update(GameContainer gc, StateBasedGame sbg, int delta)
-				throws SlickException
-		{
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public int getID()
-		{
-			return stateID;
-		}
-
+	public StateInGame(int stateID)
+	{
+		this.stateID = stateID;
 	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException
+	{
+		player = new Player();
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException
+	{
+		player.render(g);
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException
+	{
+		player.update(gc.getInput(), delta);
+	}
+
+	@Override
+	public int getID()
+	{
+		return stateID;
+	}
+
+}
