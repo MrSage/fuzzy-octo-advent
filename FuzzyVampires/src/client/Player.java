d84c08b083c6c24ec5a042095d0a7d28dcb7591a
Collin Sage <Collin Sage@141.114.165.143>
2013-02-28 13:23:13 -0500

    You can fire squares now. No hit detection still.

--- FuzzyVampires/src/client/Player.java
+++ FuzzyVampires/src/client/Player.java
@@ -1,5 +1,8 @@
 package client;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
@@ -13,13 +16,15 @@ import org.newdawn.slick.geom.Vector2f;
 
 public class Player {
 
-	private float		xspeed	= .25F, yspeed = .25F;
-	private double		theta;
-	private boolean		left, right, up, down;
+	private float					xspeed		= .25F, yspeed = .25F;
+	private double					theta;
+	private boolean					left, right, up, down;
 
-	private Vector2f	playerdirection;
-	private Shape		newhitbox;
-	private Shape		hitbox;
+	private Vector2f				playerdirection;
+	private Shape					newhitbox;
+	private Shape					hitbox;
+
+	private List<Projectile>	projectiles	= new LinkedList<Projectile>();
 
 	public Player()
 	{
@@ -34,22 +39,46 @@ public class Player {
 	public void render(Graphics g)
 	{
 		g.setColor(Color.white);
-
-		//g.draw(hitbox);
+		g.setAntiAlias(true);
 		g.draw(newhitbox);
+		//this.drawMagnitudeAndComponents(g);
+		//this.drawCenterDot(g);
+		//this.drawDebugInformation(g);
+
+		for(Projectile p: projectiles)
+		{
+			p.render(g);
+		}
+		g.setAntiAlias(false);
+	}
+
+	private void drawMagnitudeAndComponents(Graphics g)
+	{
+		// Magnitude Line
 		g.drawLine(hitbox.getCenterX(), hitbox.getCenterY(), playerdirection.x
 				+ hitbox.getCenterX(), playerdirection.y + hitbox.getCenterY());
 
+		// Y component line
 		g.drawLine(hitbox.getCenterX() + dx, hitbox.getCenterY(),
 				hitbox.getCenterX() + dx, hitbox.getCenterY() + dy);
+
+		// X component line
 		g.drawLine(hitbox.getCenterX(), hitbox.getCenterY(),
 				hitbox.getCenterX() + dx, hitbox.getCenterY());
 
+		// Line labels
 		g.drawString("" + dy, hitbox.getCenterX() + dx, hitbox.getCenterY()
 				+ dy / 2);
 		g.drawString("" + dx, hitbox.getCenterX() + dx / 2, hitbox.getCenterY());
+	}
 
+	private void drawCenterDot(Graphics g)
+	{
 		g.fillOval(Game.WIDTH / 2 - 2, Game.HEIGHT / 2 - 2, 4, 4);
+	}
+
+	private void drawDebugInformation(Graphics g)
+	{
 		g.drawString(
 				"Player coords: (" + hitbox.getCenterX() + ", "
 						+ hitbox.getCenterY() + ")", 25, 50);
@@ -59,22 +88,20 @@ public class Player {
 		g.drawString("Player direction: " + playerdirection.x + " "
 				+ playerdirection.y, 25, 65);
 
-		g.drawString("Theta: " + theta + " " + theta * 180 / Math.PI, 25, 80);
+		g.drawString("Theta: " + theta, 25, 80);
 		g.drawString("Components: " + dy + "y " + dx + "x", 25, 95);
 	}
 
 	public void update(Input input, int delta)
 	{
 		playerdirection = calculateDirectionVector(input);
-
 		Vector3f playerdirectionas3f = new Vector3f(playerdirection.x,
 				playerdirection.y, 0);
 		Vector3f horizon = new Vector3f(1, 0, 0);
 
 		theta = convertTheta(playerdirectionas3f, horizon);
 
-		newhitbox = hitbox.transform(Transform.createRotateTransform(
-				(float) theta, hitbox.getCenterX(), hitbox.getCenterY()));
+		transformHitbox(theta);
 
 		setDirections();
 
@@ -103,21 +130,63 @@ public class Player {
 			dy += yspeed;
 		}
 		move(dx * delta, dy * delta);
+
+		for(Projectile p: projectiles)
+		{
+			p.update(input, delta);
+		}
+
+		for(int i = 0; i < projectiles.size(); i++)
+		{
+			if(projectiles.get(i).isDestroyed())
+			{
+				projectiles.remove(i);
+			}
+		}
+		
+		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON))
+		{
+			projectiles.add(new Projectile(this.getX() + this.getWidth() / 2, this.getY() + this.getHeight() / 2, this
+					.getWidth() / 5, this.getHeight() / 5, playerdirection));
+		}
+		if (input.isMouseButtonDown(Input.MOUSE_RIGHT_BUTTON))
+		{
+			projectiles.add(new Projectile(this.getX() + this.getWidth() / 2, this.getY() + this.getHeight() / 2, this
+					.getWidth() / 5, this.getHeight() / 5, playerdirection));
+		}
 	}
 
 	public void move(float dx, float dy)
 	{
-		this.incrementX(dx);
-		this.incrementY(dy);
+		Vector2f direction = new Vector2f(dx, dy).normalise();
+		float xscale = direction.x;
+		float yscale = direction.y;
+		if (xscale < 0)
+		{
+			xscale = -xscale;
+		}
+		if (yscale < 0)
+		{
+			yscale = -yscale;
+		}
+
+		this.incrementX(dx * xscale);
+		this.incrementY(dy * yscale);
+	}
+
+	public void transformHitbox(double theta)
+	{
+		newhitbox = hitbox.transform(Transform.createRotateTransform(
+				(float) theta, hitbox.getCenterX(), hitbox.getCenterY()));
 	}
 
 	private void setDirections()
 	{
 
-		left = Keyboard.isKeyDown(Keyboard.KEY_LEFT);
-		right = Keyboard.isKeyDown(Keyboard.KEY_RIGHT);
-		up = Keyboard.isKeyDown(Keyboard.KEY_UP);
-		down = Keyboard.isKeyDown(Keyboard.KEY_DOWN);
+		left = Keyboard.isKeyDown(Keyboard.KEY_A);
+		right = Keyboard.isKeyDown(Keyboard.KEY_D);
+		up = Keyboard.isKeyDown(Keyboard.KEY_W);
+		down = Keyboard.isKeyDown(Keyboard.KEY_S);
 	}
 
 	float	dx;
@@ -138,7 +207,7 @@ public class Player {
 	{
 		double angle = Vector3f.angle(player, horizon);
 
-		if(player.y < 0)
+		if (player.y < 0)
 		{
 			angle = -angle;
 		}
@@ -155,9 +224,9 @@ public class Player {
 		this.hitbox.setX(hitbox.getX() + dx);
 	}
 
-	public void setX(float x)
+	public void incrementY(float dy)
 	{
-		this.hitbox.setX(x);
+		this.hitbox.setY(hitbox.getY() + dy);
 	}
 
 	public float getX()
@@ -165,9 +234,14 @@ public class Player {
 		return hitbox.getX();
 	}
 
-	public void incrementY(float dy)
+	public void setX(float x)
 	{
-		this.hitbox.setY(hitbox.getY() + dy);
+		this.hitbox.setX(x);
+	}
+
+	public float getY()
+	{
+		return hitbox.getY();
 	}
 
 	public void setY(float y)
@@ -175,8 +249,14 @@ public class Player {
 		this.hitbox.setY(y);
 	}
 
-	public float getY()
+	public float getWidth()
 	{
-		return hitbox.getY();
+		return hitbox.getWidth();
 	}
+
+	public float getHeight()
+	{
+		return hitbox.getHeight();
+	}
+
 }
---
+++ FuzzyVampires/src/client/Projectile.java
@@ -0,0 +1,87 @@
+package client;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
+import org.newdawn.slick.geom.Vector2f;
+
+public class Projectile {
+
+	private float		width, height;
+	private float		startx, starty, distance;
+	private float		decaytime	= 3 * 1000;
+
+	private boolean		destroy;
+
+	private Vector2f	direction;
+	private Shape		hitbox;
+
+	public Projectile(float x, float y, float width, float height,
+			Vector2f direction)
+	{
+		this.startx = x - width / 2;
+		this.starty = y - height / 2;
+		this.width = width;
+		this.height = height;
+
+		distance = direction.lengthSquared();
+
+		this.direction = direction.copy().normalise();
+
+		this.hitbox = new Rectangle(startx, starty, width, height);
+
+		hitbox = hitbox
+				.transform(Transform.createRotateTransform(
+						(float) direction.getTheta(), hitbox.getCenterX(),
+						hitbox.getCenterY()));
+	}
+
+	public void render(Graphics g)
+	{
+		g.setColor(new Color(1F, 1F, 1F, decaytime / 3000F));
+		g.draw(hitbox);
+	}
+
+	public void update(Input input, int delta)
+	{
+		this.decaytime -= delta;
+
+		if (decaytime <= 0)
+		{
+			this.destroy();
+		} else
+		{
+			move(direction.x * delta, direction.y * delta);
+		}
+	}
+
+	private void move(float dx, float dy)
+	{
+		if (keepMoving())
+		{
+			this.hitbox.setX(hitbox.getX() + dx);
+			this.hitbox.setY(hitbox.getY() + dy);
+		}
+	}
+
+	private boolean keepMoving()
+	{
+		float x = this.hitbox.getCenterX() - startx;
+		float y = this.hitbox.getCenterY() - starty;
+
+		return (x * x + y * y < distance);
+	}
+
+	public void destroy()
+	{
+		destroy = true;
+	}
+
+	public boolean isDestroyed()
+	{
+		return destroy;
+	}
+}
